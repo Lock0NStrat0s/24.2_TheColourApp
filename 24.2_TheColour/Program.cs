1d75b9e15c1543499eceaaa7160769f837eaa3b8
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-19 11:01:29 -0600

    Add project files.

---
+++ 24.2_TheColour/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _24._2_TheColour
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
